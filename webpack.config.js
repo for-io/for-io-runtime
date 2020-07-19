7ed33873a594cec751b27d4ac06decb6b180796b
Nikolche Mihajlovski <nikolce.mihajlovski@gmail.com>
2020-07-19 17:23:51 +0200

    Build the library in dev mode, to preserve names.

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@
 const path = require("path");
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: { index: path.resolve(__dirname, "src", "index.js") },
     output: {
         path: path.resolve(__dirname, 'dist'),
